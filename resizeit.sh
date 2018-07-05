3de77ce7e83a5b1f8c65a85f13da79d1214969ed
Lucas Lousada <lucas.lousada@hivehome.com>
2018-07-05 23:36:11 +0100

    new feature

--- resizeit.sh
+++ resizeit.sh
@@ -36,47 +36,53 @@ if [ $(which convert | wc -l) -eq 0 ]; then
         exit
 fi
 
+
+#Check files with spaces
+cd $1
+for f in *\ *; do mv "$f" "${f// /__}" 2> /dev/null ; done
+
 #Create folders
+echo "Creating DIRs..."
 mkdir -p Mogrify_images
 mkdir -p Convert_images
 mkdir -p ScaleMogrify_images
 mkdir -p KeepAspectRatio_images
 mkdir -p GM_images
-#Check files with spaces
-cd $1
-for f in *\ *; do mv "$f" "${f// /__}"; done
-cd ..
+
 #Get the files
-files=($(ls $1))
+files=($(ls -p | grep -v / ))
 
 #get information about the files
-echo "file_name,file_type,file_size,convert_TimeReal,mogrify_TimeReal,Scale_Mogrify_TimeReal,AspectRatio_convert_TimeReal,GM_TimeReal" > Files.csv
+echo "file_name,file_type,file_size,convert_TimeReal,mogrify_TimeReal,Scale_Mogrify_TimeReal,AspectRatio_convert_TimeReal,GM_TimeReal" > ../Files.csv
+echo "Converting Files..."
 for file in "${files[@]}"
 do
-	filename=$(identify $1/$file | cut -f1 -d ' ')
-	filetype=$(identify $1/$file | cut -f2 -d ' ')
-	filesize=$(identify $1/$file | cut -f3 -d ' ')
+	filename=$(identify $file | cut -f1 -d ' ')
+	filetype=$(identify $file | cut -f2 -d ' ')
+	filesize=$(identify $file | cut -f3 -d ' ')
 
 #convert using convert	
-	times=$( { time convert $1/$file -resize 500x500! Convert_images/$file ;} 2>&1)
+	times=$( { time convert $file -resize 500x500! Convert_images/$file ;} 2>&1)
 	treal=$(echo $times | cut -f2 -d ' ')
 
 #convert using mogrify
-	motimes=$( { time mogrify -resize 500x500! -path Mogrify_images $1/$file ;} 2>&1)
+	motimes=$( { time mogrify -resize 500x500! -path Mogrify_images $file ;} 2>&1)
 	motreal=$(echo $motimes | cut -f2 -d ' ')
 
 #convert using mogrify with scale
-	smotimes=$( { time mogrify -scale 500x500! -path ScaleMogrify_images $1/$file ;} 2>&1)
+	smotimes=$( { time mogrify -scale 500x500! -path ScaleMogrify_images $file ;} 2>&1)
 	smotreal=$(echo $smotimes | cut -f2 -d ' ')
 
 #convert keeping aspect ratio  and using scale+filter point
-	artimes=$( { time convert -filter point $1/$file -scale 500x500 KeepAspectRatio_images/$file ;} 2>&1)
+	artimes=$( { time convert -filter point $file -scale 500x500 KeepAspectRatio_images/$file ;} 2>&1)
 	artreal=$(echo $artimes | cut -f2 -d ' ')
 
 #convert using gm with size and scale option
-    gmtimes=$( { time gm convert -size 500x500! $1/$file -scale 500x500! GM_images/$file ; } 2>&1)
+    gmtimes=$( { time gm convert -size 500x500! $file -scale 500x500! GM_images/$file ; } 2>&1)
 	gmtreal=$(echo $gmtimes | cut -f2 -d ' ')
 
 
-	echo $filename,$filetype,$filesize,$treal,$motreal,$smotreal,$artreal,$gmtreal >> Files.csv
+	echo $filename,$filetype,$filesize,$treal,$motreal,$smotreal,$artreal,$gmtreal >> ../Files.csv
 done
+cd ..
+echo "Done."
